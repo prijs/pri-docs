a8843b5bb7946fc2d511a1db4c53d2f15e682a24
ziyi.hzy <ziyi.hzy@alibaba-inc.com>
2018-03-25 16:52:01 +0800

    Updates

--- 8.chunk.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{144:function(t,n,e){"use strict";e.r(n),n.default="# Tslint support\n\nAfter `pri` was installed, [tslint](https://www.npmjs.com/package/tslint) will take effect automatically.\n\n> It's enforce to check when execute `npm run build`, so you can't skip check!\n"}}]);
---
+++ automaticOptimizationTslintSupport.chunk.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{142:function(t,n,e){"use strict";e.r(n),n.default="# Tslint support\n\nAfter `pri` was installed, [tslint](https://www.npmjs.com/package/tslint) will take effect automatically.\n\n> It's enforce to check when execute `npm run build`, so you can't skip check!\n"}}]);
