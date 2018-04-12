52411f38b957ecc63dde610c6ba402fe7828e8da
ziyi.hzy <ziyi.hzy@alibaba-inc.com>
2018-04-12 19:44:14 +0800

    Updates

---
+++ sw.js
@@ -0,0 +1,39 @@
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+// Get mock list start
+
+var allMocks = {};
+var mockList = [];
+
+Object.keys(allMocks).forEach(mockKey => {
+  var mock = allMocks[mockKey];
+  mockList.push({ url: new URL(mockKey, location), value: mock });
+});
+// Get mock list end
+
+self.addEventListener('fetch', event => {
+  var requestUrl = new URL(event.request.url);
+
+  const mockInfo = mockList.find(
+    mock =>
+      requestUrl.hostname === mock.url.hostname &&
+      requestUrl.pathname === mock.url.pathname
+  );
+
+  if (mockInfo) {
+    var responseInit = {
+      status: 200,
+      statusText: 'OK',
+      headers: { 'Content-Type': 'application/json' }
+    };
+
+    var responseBody =
+      typeof mockInfo.value === 'function' ? mockInfo.value() : mockInfo.value;
+
+    var mockResponse = new Response(JSON.stringify(responseBody), responseInit);
+
+    event.respondWith(mockResponse);
+  }
+});
