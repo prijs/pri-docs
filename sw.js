4f020e0eda6a25acb2e0559f2c8a90b760386962
ziyi.hzy <ziyi.hzy@alibaba-inc.com>
2018-04-13 16:23:11 +0800

    Updates

--- sw.js
+++ sw.js
@@ -2,6 +2,10 @@ self.addEventListener('install', event => {
   self.skipWaiting();
 });
 
+self.addEventListener('activate', event => {
+  self.clients.claim();
+});
+
 // Get mock list start
 
 var allMocks = {};
@@ -36,4 +40,54 @@ self.addEventListener('fetch', event => {
 
     event.respondWith(mockResponse);
   }
+});
+
+var BUNDLE_VERSION = 'v1';
+
+var bundleCaches = [
+  '/pri-docs/automaticOptimizationAutoCreateProjectFiles.519c.chunk.js',
+  '/pri-docs/automaticOptimizationAutoDlls.519c.chunk.js',
+  '/pri-docs/automaticOptimizationAutoPickSharedModules.519c.chunk.js',
+  '/pri-docs/automaticOptimizationAutoPrefetch.519c.chunk.js',
+  '/pri-docs/automaticOptimizationAutomaticCodeSplitting.519c.chunk.js',
+  '/pri-docs/automaticOptimizationAutomaticHmr.519c.chunk.js',
+  '/pri-docs/automaticOptimizationImportOnDemand.519c.chunk.js',
+  '/pri-docs/automaticOptimizationScopeHoist.519c.chunk.js',
+  '/pri-docs/automaticOptimizationTreeShaking.519c.chunk.js',
+  '/pri-docs/automaticOptimizationTslintSupport.519c.chunk.js',
+  '/pri-docs/automaticOptimizationTypescriptSupport.519c.chunk.js',
+  '/pri-docs/config.519c.chunk.js',
+  '/pri-docs/developmentBuild.519c.chunk.js',
+  '/pri-docs/developmentCommands.519c.chunk.js',
+  '/pri-docs/developmentContext.519c.chunk.js',
+  '/pri-docs/developmentDevService.519c.chunk.js',
+  '/pri-docs/developmentProject.519c.chunk.js',
+  '/pri-docs/developmentServiceWorker.519c.chunk.js',
+  '/pri-docs/developmentTest.519c.chunk.js',
+  '/pri-docs/developmentWebUi.519c.chunk.js',
+  '/pri-docs/developmentWriteAPlugin.519c.chunk.js',
+  '/pri-docs/featuresBuiltInDataStream.519c.chunk.js',
+  '/pri-docs/featuresDeployToGithubPages.519c.chunk.js',
+  '/pri-docs/featuresDynamicImport.519c.chunk.js',
+  '/pri-docs/featuresEnvironmentVariable.519c.chunk.js',
+  '/pri-docs/featuresMarkdownSupport.519c.chunk.js',
+  '/pri-docs/featuresMock.519c.chunk.js',
+  '/pri-docs/featuresPageNotFound.519c.chunk.js',
+  '/pri-docs/featuresProjectDashboard.519c.chunk.js',
+  '/pri-docs/featuresScssLessCss.519c.chunk.js',
+  '/pri-docs/featuresTestAndCodeCoverage.519c.chunk.js',
+  '/pri-docs/layoutSupport.519c.chunk.js',
+  '/pri-docs/pagesAreRoutes.519c.chunk.js',
+  '/pri-docs/pluginsPriPluginDob.519c.chunk.js',
+  '/pri-docs/index.519c.chunk.js',
+  '/pri-docs/main.519c.js',
+  '/pri-docs/main.519c.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(BUNDLE_VERSION).then(cache => {
+      return cache.addAll(bundleCaches);
+    })
+  );
 });
