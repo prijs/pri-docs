727105169c432aa78df905b6e5910ae983cec465
ziyi.hzy <ziyi.hzy@alibaba-inc.com>
2018-04-15 18:09:58 +0800

    Updates

--- sw.js
+++ sw.js
@@ -43,50 +43,49 @@ self.addEventListener('fetch', event => {
 });
 
 var BUNDLE_PREFIX = '__bundle__';
-var BUNDLE_VERSION = BUNDLE_PREFIX + '17cb';
+var BUNDLE_VERSION = BUNDLE_PREFIX + '3786';
 
 var bundleCaches = [
-  '/pri-docs/automaticOptimizationAutoCreateProjectFiles.17cb.chunk.js',
-  '/pri-docs/automaticOptimizationAutoDlls.17cb.chunk.js',
-  '/pri-docs/automaticOptimizationAutoPickSharedModules.17cb.chunk.js',
-  '/pri-docs/automaticOptimizationAutoPrefetch.17cb.chunk.js',
-  '/pri-docs/automaticOptimizationAutomaticCodeSplitting.17cb.chunk.js',
-  '/pri-docs/automaticOptimizationAutomaticHmr.17cb.chunk.js',
-  '/pri-docs/automaticOptimizationImportOnDemand.17cb.chunk.js',
-  '/pri-docs/automaticOptimizationScopeHoist.17cb.chunk.js',
-  '/pri-docs/automaticOptimizationTreeShaking.17cb.chunk.js',
-  '/pri-docs/automaticOptimizationTslintSupport.17cb.chunk.js',
-  '/pri-docs/automaticOptimizationTypescriptSupport.17cb.chunk.js',
-  '/pri-docs/config.17cb.chunk.js',
-  '/pri-docs/developmentBuild.17cb.chunk.js',
-  '/pri-docs/developmentCommands.17cb.chunk.js',
-  '/pri-docs/developmentContext.17cb.chunk.js',
-  '/pri-docs/developmentDevService.17cb.chunk.js',
-  '/pri-docs/developmentProject.17cb.chunk.js',
-  '/pri-docs/developmentServiceWorker.17cb.chunk.js',
-  '/pri-docs/developmentTest.17cb.chunk.js',
-  '/pri-docs/developmentWebUi.17cb.chunk.js',
-  '/pri-docs/developmentWriteAPlugin.17cb.chunk.js',
-  '/pri-docs/featuresBuiltInDataStream.17cb.chunk.js',
-  '/pri-docs/featuresDeployToGithubPages.17cb.chunk.js',
-  '/pri-docs/featuresDynamicImport.17cb.chunk.js',
-  '/pri-docs/featuresEnvironmentVariable.17cb.chunk.js',
-  '/pri-docs/featuresMarkdownSupport.17cb.chunk.js',
-  '/pri-docs/featuresMock.17cb.chunk.js',
-  '/pri-docs/featuresPageNotFound.17cb.chunk.js',
-  '/pri-docs/featuresProjectDashboard.17cb.chunk.js',
-  '/pri-docs/featuresScssLessCss.17cb.chunk.js',
-  '/pri-docs/featuresTestAndCodeCoverage.17cb.chunk.js',
-  '/pri-docs/layoutSupport.17cb.chunk.js',
-  '/pri-docs/pagesAreRoutes.17cb.chunk.js',
-  '/pri-docs/pluginsPriPluginDob.17cb.chunk.js',
-  '/pri-docs/index.17cb.chunk.js',
-  '/pri-docs/main.17cb.js',
-  '/pri-docs/main.17cb.css'
+  '/pri-docs/automaticOptimizationAutoCreateProjectFiles.3786.chunk.js',
+  '/pri-docs/automaticOptimizationAutoDlls.3786.chunk.js',
+  '/pri-docs/automaticOptimizationAutoPickSharedModules.3786.chunk.js',
+  '/pri-docs/automaticOptimizationAutoPrefetch.3786.chunk.js',
+  '/pri-docs/automaticOptimizationAutomaticCodeSplitting.3786.chunk.js',
+  '/pri-docs/automaticOptimizationAutomaticHmr.3786.chunk.js',
+  '/pri-docs/automaticOptimizationImportOnDemand.3786.chunk.js',
+  '/pri-docs/automaticOptimizationScopeHoist.3786.chunk.js',
+  '/pri-docs/automaticOptimizationTreeShaking.3786.chunk.js',
+  '/pri-docs/automaticOptimizationTslintSupport.3786.chunk.js',
+  '/pri-docs/automaticOptimizationTypescriptSupport.3786.chunk.js',
+  '/pri-docs/config.3786.chunk.js',
+  '/pri-docs/developmentBuild.3786.chunk.js',
+  '/pri-docs/developmentCommands.3786.chunk.js',
+  '/pri-docs/developmentContext.3786.chunk.js',
+  '/pri-docs/developmentDevService.3786.chunk.js',
+  '/pri-docs/developmentProject.3786.chunk.js',
+  '/pri-docs/developmentServiceWorker.3786.chunk.js',
+  '/pri-docs/developmentTest.3786.chunk.js',
+  '/pri-docs/developmentWebUi.3786.chunk.js',
+  '/pri-docs/developmentWriteAPlugin.3786.chunk.js',
+  '/pri-docs/featuresBuiltInDataStream.3786.chunk.js',
+  '/pri-docs/featuresDeployToGithubPages.3786.chunk.js',
+  '/pri-docs/featuresDynamicImport.3786.chunk.js',
+  '/pri-docs/featuresEnvironmentVariable.3786.chunk.js',
+  '/pri-docs/featuresMarkdownSupport.3786.chunk.js',
+  '/pri-docs/featuresMock.3786.chunk.js',
+  '/pri-docs/featuresPageNotFound.3786.chunk.js',
+  '/pri-docs/featuresProjectDashboard.3786.chunk.js',
+  '/pri-docs/featuresScssLessCss.3786.chunk.js',
+  '/pri-docs/featuresTestAndCodeCoverage.3786.chunk.js',
+  '/pri-docs/layoutSupport.3786.chunk.js',
+  '/pri-docs/pagesAreRoutes.3786.chunk.js',
+  '/pri-docs/pluginsPriPluginDob.3786.chunk.js',
+  '/pri-docs/index.3786.chunk.js',
+  '/pri-docs/main.3786.js',
+  '/pri-docs/main.3786.css'
 ];
 
 self.addEventListener('install', event => {
-  console.log('install');
   event.waitUntil(
     caches.open(BUNDLE_VERSION).then(cache => {
       return cache.addAll(bundleCaches);
@@ -98,17 +97,13 @@ self.addEventListener('install', event => {
  * Delete all bundle caches except current BUNDLE_VERSION.
  */
 self.addEventListener('activate', event => {
-  console.log('activate');
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames
           .filter(cacheName => cacheName.startsWith(BUNDLE_PREFIX))
           .filter(cacheName => cacheName !== BUNDLE_VERSION)
-          .map(cacheName => {
-            console.log('delete cacheName');
-            caches.delete(cacheName);
-          })
+          .map(cacheName => caches.delete(cacheName))
       );
     })
   );
