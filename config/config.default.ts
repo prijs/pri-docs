8edee6789626d6f8bade7f82643e7a38ca9025b9
ascoders <576625322@qq.com>
2018-04-18 20:01:32 +0800

    update

--- config/config.default.ts
+++ config/config.default.ts
@@ -3,5 +3,7 @@ import { ProjectConfig } from 'pri/client';
 export default {
   title: 'Pri',
   publicPath: '/pri-docs',
-  baseHref: '/pri-docs'
+  baseHref: '/pri-docs',
+  useServiceWorker: true,
+  prefetchChunks: true
 } as ProjectConfig;
