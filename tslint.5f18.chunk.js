3c72cf5ee5036e6fb53c915d3c401fb1c12a802e
ziyi.hzy <ziyi.hzy@alibaba-inc.com>
2018-04-27 12:22:53 +0800

    Updates

--- tslint.5f18.chunk.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[33],{180:function(e,n){e.exports='<h1>Tslint</h1>\n<p>After <code>pri</code> was installed, <a href="https://www.npmjs.com/package/tslint">tslint</a> will take effect automatically.</p>\n<blockquote>\n<p>It’s enforce to check when execute <code>npm run build</code>, so you can’t skip check!</p>\n</blockquote>\n'}}]);
--- typescript.5f18.chunk.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[34],{181:function(p,i){p.exports="<h1>Typescript</h1>\n<p><code>pri</code> is written by typescript, so it’s easy to use in typescript project.</p>\n<p>In fact, all project files can use TS files, including configuration files.</p>\n"}}]);
