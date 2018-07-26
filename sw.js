288c80d17df764aafef3ed703a1bd9b88173ed9c
ziyi.hzy <ziyi.hzy@alibaba-inc.com>
2018-07-26 17:13:08 +0800

    Updates

--- sw.js
+++ sw.js
@@ -51,7 +51,7 @@ self.addEventListener('fetch', event => {
 });
 
 var SSR_BUNDLE_PREFIX = '__ssr_bundle__';
-var SSR_BUNDLE_VERSION = SSR_BUNDLE_PREFIX + 'a4b9';
+var SSR_BUNDLE_VERSION = SSR_BUNDLE_PREFIX + '9a44';
 
 var currentCacheSsrRequest = null;
 var currentCacheSsrOriginHtml = null;
